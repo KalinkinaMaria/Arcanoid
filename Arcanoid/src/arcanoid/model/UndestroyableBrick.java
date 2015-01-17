c433bc61ac69e04d6263f540fc64453ddc226d66
KalinkinaMaria <mashyfycya@gmail.com>
2015-01-17 22:34:57 +0300

    Format UndestroyableBrick

--- Arcanoid/src/arcanoid/model/UndestroyableBrick.java
+++ Arcanoid/src/arcanoid/model/UndestroyableBrick.java
@@ -11,17 +11,26 @@ import com.golden.gamedev.object.Sprite;
 
 
 /**
- *
+ * Неразрушаемый кирпич
  * @author Елена
  */
 public class UndestroyableBrick extends Brick implements Bounced {
 
+    /**
+     * Конструктор
+     * @param table таблица соответствия спрайтов и элементов 
+     */
     public UndestroyableBrick(Buffer table) {
         super(table);
     }
 
+    /**
+     * Клонирование элемента
+     * @return клонированный элемент
+     */
     public UndestroyableBrick clone() {
         UndestroyableBrick brick = new UndestroyableBrick(this.table);
+        // Клонирование спрайтов
         Sprite sprite = table.getSprite(this);
         table.addPair(brick, sprite);
         brick.copy(this);
