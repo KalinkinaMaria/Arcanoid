b9f6d629519f8cd45a5417cc757de426362c00e2
LepilkinaElena <eklepilkina@gmail.com>
2015-01-17 21:51:06 +0300

    Format Arcanoid

--- Arcanoid/src/arcanoid/Arcanoid.java
+++ Arcanoid/src/arcanoid/Arcanoid.java
@@ -7,9 +7,6 @@ package arcanoid;
 
 import arcanoid.events.AttemptStartedEvent;
 import arcanoid.events.AttemptStartedListener;
-import arcanoid.events.GameStateChangeEvent;
-import arcanoid.events.GameStateChangeListener;
-import arcanoid.model.CollisionHandler;
 import arcanoid.service.Buffer;
 import arcanoid.service.SpeedVector;
 import arcanoid.view.Ambiance;
@@ -19,7 +16,6 @@ import com.golden.gamedev.object.Background;
 import com.golden.gamedev.object.CollisionManager;
 import com.golden.gamedev.object.GameFont;
 import com.golden.gamedev.object.PlayField;
-import com.golden.gamedev.object.SpriteGroup;
 import com.golden.gamedev.object.background.ImageBackground;
 import java.awt.Dimension;
 import java.awt.Graphics2D;
@@ -27,19 +23,29 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- *
+ * Главный класс игры
+ * 
  * @author Мария
  */
 public class Arcanoid  extends Game implements AttemptStartedListener {
-    private PlayField        playfield;
-    private Background       background;
+    /** Игровое поле GTGE*/
+    private PlayField playfield;
+    /** Фон*/
+    private Background background;
+    /** Модель игры*/
     private GameModel gameModel;
+    /** Предыдущая позиция мыши(для движения)*/
     private int oldMousePosition;
+    /** Обстановка, отвечающая за графичечкое отображение элементов*/
     private Ambiance ambiance;
-    private GameFont           font;
+    /** Шрифт для вывода сообщений*/
+    private GameFont font;
+    /** Сообщение для пользователя*/
     private String message;
     
+    // Раскомментировать, что убрать лишние счетчики и надписи
     //{ distribute = true; }
+    
     /**
      * @param args the command line arguments
      */
@@ -49,68 +55,104 @@ public class Arcanoid  extends Game implements AttemptStartedListener {
         game.start();
     }
 
+    /**
+     * Инициализация ресурсов игры
+     */
     @Override
     public void initResources() {
         // Создать буффер для связи логики и отображения.
         Buffer buffer = new Buffer();
+        // Инициализация полей
         playfield = new PlayField();
         message = "";
         ambiance = new Ambiance(buffer);
         gameModel = new GameModel(buffer);
+        // Связать модель и обстановку с помощью сигналов дляобщения в дальнейшем
         gameModel.createConnectionWithField(ambiance);
         gameModel.startGame();
         oldMousePosition  = this.getMouseX();
+        // Задать фон
         background = new ImageBackground(getImage("img/1.jpg"), 1000, 600);
         playfield.setBackground(background);
         gameModel.addAttemptStartedListener(this);
+        // Зарегистрировать существующие группы спрайтов
         ambiance.registerSpriteGroups(playfield);
         hideCursor();
+        // Установить правила столкновений игры
         gameModel.registerCollisionRules(ambiance);
         ambiance.setCollisionBounds(playfield);
-        
+        // Установить связь с помощью сигналов с элементом, который отвечает за провал игры
         ambiance.setConnectionWithGhangingGameStateElement(gameModel);
+        // Получение шрифта из картинки
         String fontString = "ABCDEFGHIJKLMNOPQRSTUVXWYZabcdefghijklmnopqrstuvxwyz1234567890aaaaaaaaa?!a.:- ";
         font = fontManager.getFont(getImages("img/font.png", 26, 3), fontString);
     }
 
+    /**
+     * Обновить игру
+     * 
+     * @param l время обновления
+     */
     @Override
     public void update(long l) {
+        // Обновить поле
         playfield.update(l);
         // Запуск мячика
         if (click() && !gameModel.isGameStarted()) {
+            // Начать попытку
             gameModel.startAttempt();           
             ambiance.setAttemptCollisionBounds(playfield);
+            // Установить коллизии между объектами
             ambiance.setCollisionObjects(playfield);
         }
+        // Проверить движение мышки.
         checkMouseMoving(l);
-        //System.out.println(l);
     }
 
+    /**
+     * Отрисовка
+     * @param gd графика
+     */
     @Override
     public void render(Graphics2D gd) {
-        playfield.render(gd);
-        HashMap<String, String> gameData = gameModel.getGameData();
         String text = "";
+        // Отрисовать поле
+        playfield.render(gd);
+        // Получить данные игры(жизни, очки)
+        HashMap<String, String> gameData = gameModel.getGameData();
+        // Создать текст по полученным данным
         for (Map.Entry<String, String> entrySet : gameData.entrySet()) {
             String key = entrySet.getKey();
             String value = entrySet.getValue();
             text += key + ": " + value + " ";
         }
+        // Отрисовать текст
         font.drawText(gd, text, GameFont.LEFT, 840, 30, 150, 10, 0);
         if (!message.isEmpty()) {
             font.drawString(gd, message, 370, 300);
         }
     }
     
+    /**
+     * Проверить дфижение мыши
+     * 
+     * @param l время на момент проверки
+     */
     public void checkMouseMoving(long l) {
         // Игрок сделал движение мышкой.
         if (this.getMouseX() != oldMousePosition && l != 0) {
+            // Обработать действия игрока
             gameModel.processPlayerAction(getMouseSpeed(l));
             oldMousePosition = this.getMouseX();
         }
     }
     
-    public SpeedVector getMouseSpeed(long l) {
+    /**
+     * Получить скорость движения мыши
+     * @param l время движения
+     * @return вектор скорости
+     */
+    private SpeedVector getMouseSpeed(long l) {
         SpeedVector result;
         result = new SpeedVector(2*(this.getMouseX() - oldMousePosition)/l, 0);
         return result;
@@ -121,23 +163,34 @@ public class Arcanoid  extends Game implements AttemptStartedListener {
         
     }
 
+    /**
+     * Вернуться на стратовую позицию
+     * 
+     * @param e событие окончания попытки
+     */
     @Override
     public void returnToStartPosition(AttemptStartedEvent e) {
         CollisionManager[] collisionGroups = playfield.getCollisionGroups();
+        // Удалить старые коллизионные группы
         for (CollisionManager manager: collisionGroups) {
             playfield.removeCollisionGroup(manager);
         }
+        // Настроить новые
         ambiance.setCollisionBounds(playfield);
         ambiance.setCollisionObjects(playfield);
     }
     
-    public void endGame(boolean success) {
-        
+    /**
+     * Закончить игру
+     * 
+     * @param success результат игры
+     */
+    @Override
+    public void endGame(boolean success) {        
         if (success) {
             message = "You win!!!";
         } else {
             message = "You lose!!!";
         }
-
     }
 }
